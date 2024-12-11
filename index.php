346f4edecee81c5ec69aac3019553bd8254a594b
Abel Valdes <abelalejandrovaldes22@gmail.com>
2024-12-11 23:29:56 +0100

    Firts Commit
    indexx+header+footer

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+<?php include 'resources/header.php'; ?>
+
+<main>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the main content of the page.</p>
+</main>
+
+<?php include 'resources/footer.php'; ?>
+</body>
+</html>
---
+++ resources/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="container">
+        <div class="socials">
+            <p>¡Síguenos!</p>
+            <a href="#">
+                <img src="img/Social Icons tiktok.png" alt="TikTok">
+            </a>
+            <a href="#">
+              <img src="img/Social Icons instagram.png" alt="Instagram">
+            </a>
+        </div>
+
+        <div class="brand">
+            <img src="img/LOGO PAWTS.png" alt="Logo Paws">
+        </div>
+
+        <div class="links">
+            <a href="#">Política de Privacidad</a>
+            <a href="#">Sobre Nosotros</a>
+            <a href="#">Contáctanos</a>
+            <a href="#">Centro de Ayuda</a>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <p>Copyright PAWTS. © Todos los derechos reservados.</p>
+    </div>
+</footer>
