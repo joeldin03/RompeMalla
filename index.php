e95998f6b19c91ebfc4c769257d352a319369df5
joeldin03 <jlozanobarbancho22@outlook.com>
2024-12-13 22:38:43 +0100

    update

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
-    <title>PAWTS</title>
+    <link rel="icon" href="img/logo chiquito.png">
+    <title>Pawts</title>
 </head>
 
 <body>
