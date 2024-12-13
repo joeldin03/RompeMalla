4f13f9dc7358259289df6d63c24bd3f1a4b9fab7
joeldin03 <jlozanobarbancho22@outlook.com>
2024-12-13 22:53:39 +0100

    favicom añadido

--- pages/contacto.php
+++ pages/contacto.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/RompeMalla/css/contacto.css">
     <link rel="stylesheet" href="/RompeMalla/css/index.css">
-    
+    <link rel="icon" href="img/logo chiquito.png">
     <title>PAWTS</title>
 </head>
 
---
+++ pages/privacidad.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="img/logo chiquito.png">
+    <title>Pawts</title>
+</head>
+
+<body>
+    <?php include '../resources/header.php'; ?>
+
+    <main>
+        <h1>POLÍTICA DE PRIVACIDAD</h1>
+        <p>PAWTS valora la privacidad y la seguridad de sus usuarios.</p>
+    </main>
+
+    <?php include '../resources/footer.php'; ?>
+</body>
+
+</html>
