781c384a99565891d23a3cb1ece22fd5eb146bd4
joeldin03 <jlozanobarbancho22@outlook.com>
2024-12-27 18:47:04 +0100

    faq updated

--- pages/FAQ.php
+++ pages/FAQ.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FAQ - PAWTS</title>
     <link rel="stylesheet" href="/RompeMalla/css/FAQ.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
 </head>
 <body>
 
