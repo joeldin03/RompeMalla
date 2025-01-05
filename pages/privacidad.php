a496f1e5b4dcf5139caf3f6109571d7e3971a375
joeldin03 <jlozanobarbancho22@outlook.com>
2025-01-05 07:43:52 +0100

    privacidad primer commit

--- pages/privacidad.php
+++ pages/privacidad.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/RompeMalla/css/privacidad.css">
     <link rel="stylesheet" href="css/index.css">
-    <link rel="icon" href="img/logo chiquito.png">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/logo chiquito.png">
     <title>Pawts</title>
 </head>
 
@@ -15,6 +17,61 @@
     <main>
         <h1>POLÍTICA DE PRIVACIDAD</h1>
         <p>PAWTS valora la privacidad y la seguridad de sus usuarios.</p>
+        <p>Esta Política de Privacidad describe cómo recopilamos, usamos y protegemos la información personal que compartes con nosotros a través de nuestra aplicación y página web, cumpliendo con el Reglamento General de Protección de Datos (GDPR) y utilizando tecnología de SSL para proteger tus datos.</p>
+        
+        <h2>INFORMACIÓN QUE RECOPILAMOS</h2>
+        <ol>
+            <li>Información Personal: Nombre, correo electrónico, dirección, número de teléfono, y detalles de pago cuando te registras o realizas una compra en PAWTS.</li>
+            <li>Información de Uso: Datos sobre cómo usas nuestra aplicación y página web, como las páginas que visitas, el tiempo que pasas en ellas y las acciones que realizas.</li>
+            <li>Información de la Mascota: Información sobre tus mascotas, como nombre, raza, fecha de nacimiento, historial médico y otros detalles relevantes que ingresas en la app.</li>
+            <li>Datos de Dispositivo: Información técnica como dirección IP, tipo de dispositivo, sistema operativo y navegador utilizado.</li>
+        </ol>
+
+        <h2>CÓMO USAMOS LA INFORMACIÓN</h2>
+        <p>Proveer y Mejorar el Servicio: Utilizamos tus datos para personalizar tu experiencia, brindarle servicios personalizados y mejorar la funcionalidad de nuestra app.</p>
+        <ol>
+            <li>Procesamiento de Pagos: Para gestionar tus compras y transacciones de manera segura.</li>
+            <li>Comunicaciones: Enviar actualizaciones sobre la app, notificaciones de eventos, ofertas especiales y novedades de PAWTS.</li>
+            <li>Análisis y Estadísticas: Para entender cómo los usuarios interactúan con la app y mejorar nuestro servicio</li>
+        </ol>
+
+        <h2>CÓMO COMPARTIMOS LA INFORMACIÓN</h2>
+        <p>Con Proveedores de Servicios: Trabajamos con terceros que nos ayudan a brindar nuestros servicios (procesadores de pago, servicios de correo electrónico, etc.). Estos proveedores sólo tienen acceso a la información necesaria para realizar sus funciones.</p>
+        <ol>
+            <li>Obligaciones Legales: PAWTS podrá compartir tu información si es requerido por ley o para proteger los derechos y seguridad de nuestros usuarios.</li>
+        </ol>
+
+        <h2>TUS DERECHOS DE PRIVACIDAD</h2>
+        <p>Bajo el GDPR, tienes los siguientes derechos:</p>
+        <ol>
+            <li>Acceso: Puedes solicitar una copia de los datos personales que tenemos sobre ti.</li>
+            <li>Rectificación: Puedes corregir cualquier información incorrecta o incompleta.</li>
+            <li>Eliminación: Solicitar que eliminemos tus datos personales en ciertas circunstancias.</li>
+            <li>Restricción del Tratamiento: Puedes limitar la manera en que utilizamos tus datos.</li>
+            <li>Portabilidad: Tienes derecho a recibir tus datos personales en un formato estructurado y legible.</li>
+        </ol>
+        <p>Para ejercer tus derechos, contáctanos en soporte@pawts.com</p>
+
+        <h2>SEGURIDAD DE LOS DATOS</h2>
+        <p>Usamos el Certificado SSL (Secure Sockets Layer) para asegurar que toda la información transmitida entre tu dispositivo y nuestro servidor esté encriptada y protegida.</p>
+        <ol>
+            <li>Tomamos medidas de seguridad adicionales, como controles de acceso y cifrado de datos, para asegurar la protección de tu información.</li>
+        </ol>
+
+        <h2>RETENCIÓN DE DATOS</h2>
+        <ol>
+            <li>Conservamos tus datos personales durante el tiempo que sea necesario para brindarte nuestros servicios o según lo requiera la ley.</li>
+            <li>Los datos de la cuenta de usuario se eliminan automáticamente si no ha habido actividad durante más de 24 meses</li>
+        </ol>
+
+        <h2>COOKIES Y TECNOLOGÍA DE SEGUIMIENTO</h2>
+        <ol>
+            <li>Utilizamos cookies para mejorar tu experiencia, recordar tus preferencias y analizar el uso de la app y la web.</li>
+            <li>Puedes gestionar tus preferencias de cookies a través de la configuración de tu navegador</li>
+        </ol>
+
+        <h2>CONTACTO</h2>
+        <p>Si tienes preguntas sobre nuestra Política de Privacidad, contáctanos en soporte@pawts.com</p>
     </main>
 
     <?php include '../resources/footer.php'; ?>
