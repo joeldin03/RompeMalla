a58a1f55119e2997499605d2e39df8ea45323576
joeldin03 <jlozanobarbancho22@outlook.com>
2025-02-15 00:03:26 +0100

    arreglando cositas

--- pages/Asociaciones.php
+++ pages/Asociaciones.php
@@ -6,6 +6,7 @@
     <title>Asociaciones y Colaboraciones</title>
     <link rel="stylesheet" href="/RompeMalla/css/Asociaciones.css">
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@100..900&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/logo chiquito.png">
 
 </head>
@@ -14,15 +15,19 @@
 <?php include '../resources/header.php'; ?>
 
     <div class="hero">
-        <img src="../img/vacas.png" alt="Fondo vaca" class="hero-img">
+        <img src="../img/vacas.png" alt="Fondo vaca">
         <div class="hero-text">
-            <h1>Nuestras Asociaciones y Colaboraciones</h1>
-            <p>En <strong>PAWTS</strong>, estamos orgullosos de trabajar con marcas y profesionales que comparten nuestra pasión por el cuidado y bienestar de las mascotas. Descubre nuestras asociaciones y colaboradores que hacen posible que sigamos creciendo y ofreciendo un servicio de calidad a nuestra comunidad.</p>
+            <h1 style="color: #007f6e">Nuestras Asociaciones y Colaboraciones</h1>
+            <br>
+            <p>En <span style="color: #ff4a92;">PAWTS</span>, estamos orgullosos de trabajar con marcas y profesionales que comparten nuestra pasión por el cuidado y bienestar de las mascotas.<br>
+            <br>Descubre nuestras asociaciones y colaboradores que hacen posible que sigamos creciendo y ofreciendo un servicio de calidad a nuestra comunidad.</p>
         </div>
     </div>
 
     <section class="colaboradores">
-        <h2>COLABORADORES DE PAWTS</h2>
+        <div style="background-color: #D5F1E9;">
+            <h2 style="color: #ff4a92;">COLABORADORES DE PAWTS</h2>
+        </div>
         <div class="cards-container">
             <?php
             for ($i = 0; $i < 9; $i++) {
@@ -40,10 +45,11 @@
         </div>
     </section>
 
-    <section class="contacto">
-        <p>¿Te gustaría colaborar con <strong>PAWTS</strong>? ¡Contáctanos y sé parte de nuestra comunidad de socios!</p>
-        <a href="#" class="button">Contáctanos</a>
-    </section>
+    <div class="contacto">
+        <p>¿Te gustaría colaborar con PAWTS?<br>
+        ¡Contáctanos y sé parte de nuestra comunidad de socios!</p>
+    </div>
+    <a href="mailto:soporte@pawts.com" class="button">Contáctanos</a>
 
     <?php include '../resources/footer.php'; ?>
 </body>
