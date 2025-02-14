8f5d1fae5b27494672fcf13c0e64beae1eabc172
joeldin03 <jlozanobarbancho22@outlook.com>
2025-02-14 02:47:44 +0100

    updated

--- pages/servicios.php
+++ pages/servicios.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/servicios.css">
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/logo chiquito.png">
