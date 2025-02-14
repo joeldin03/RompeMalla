bf2f654e04138fb4589b5abe52f766a95280a845
joeldin03 <jlozanobarbancho22@outlook.com>
2025-02-14 02:47:52 +0100

    added new page

---
+++ pages/equipo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/equipo.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/logo chiquito.png">
+    <title>Pawts - Equipo</title>
+</head>
+<body>
+    <?php include '../resources/header.php'; ?>
+    <h1>Conoce al equipo:</h1>
+    <section class="teams">
+        <div class="container">
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">                
+                <h3>FERNANDO</h3>
+                <p>FUNDADOR</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">                
+                <h3>ALEXANDER</h3>
+                <p>PROGRAMADOR</p>
+            </div>
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">
+                <h3>JONATAN</h3>
+                <p>PROGRAMADOR</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">                
+                <h3>RUT</h3>
+                <p>DISSEÑADORA</p>
+            </div>
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">
+                <h3>ANAÏS</h3>
+                <p>DISEÑADORA</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="team">
+                <img src="../img/hamstergafa.png" alt="profile pic">                
+                <h3>ADRIÁN</h3>
+                <p>ESPECIALISTA EN MARKETING</p>
+            </div>
+        </div>
+    </section>
+    <?php include '../resources/footer.php'; ?>
+</body>
+</html>
