6095f855a7b010934f16c135d4ac8cebc7c4e50c
joeldin03 <jlozanobarbancho22@outlook.com>
2025-02-17 00:47:08 +0100

    new page (unfinished)

---
+++ pages/membresias.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membresías - PAWTS</title>
+    <link rel="stylesheet" href="../css/membresias.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/logo chiquito.png">
+</head>
+<body>
+    <?php include '../resources/header.php'; ?>
+
+    <h1>Tipos de Membresías</h1>
+    <h2>Elige el Plan Perfecto para Ti y Tu Mascota</h2>
+    <p>Ya sea que estés buscando un plan gratuito o quieras disfrutar de todas las funciones Premium, PAWTS
+        tiene la opción perfecta para ti y tu mascota.
+        <span class="highlight">¡Descubre nuestros planes y sus beneficios!</span>
+    </p>
+
+    <div class="plans">
+            <div class="plan-pro">
+                <h1>PRO</h1>
+                <div class="prices">
+                    <strong>9,99 €/mes<br>
+                    27,99 €/tres meses<br>
+                    99,99 €/año</strong>
+                </div>
+                <p>Hasta <strong>20</strong> perfiles</p>
+                <p>Calendario de Citas: <strong>Sí</strong></p>
+                <p>Acceso a Pegatinas y Fondos: <strong>Premium</strong></p>
+                <p>Acceso a Eventos: <br><strong>Sí, Descuentos y Prioridad</strong></p>
+                <p>Sin anuncios: <strong>Sí</strong></p>
+                <p>Monedas PAWTS mensuales: <strong>Sí</strong></p>
+            </div>
+            <div class="plan-premium">
+                <h1>PREMIUM</h1>
+                <div class="prices">
+                    <strong>4,99 €/mes<br>
+                    13,99 €/tres meses<br>
+                    49,99 €/año</strong>
+                </div>
+                <p>Hasta <strong>3</strong> perfiles</p>
+                <p>Calendario de Citas: <strong>Sí</strong></p>
+                <p>Acceso a Pegatinas y Fondos: <strong>Premium</strong></p>
+                <p>Acceso a Eventos: <br><strong>Sí, Descuentos y Prioridad</strong></p>
+                <p>Sin anuncios: <strong>Sí</strong></p>
+                <p>Monedas PAWTS mensuales: <strong>Sí</strong></p>
+            </div>
+            <div class="plan-basic">
+                <h1>BASIC</h1>
+                <div class="prices">
+                    <strong><br>GRATIS<br><br></strong>
+                </div>
+                <p>Hasta <strong>3</strong> perfiles</p>
+                <p>Calendario de Citas: <strong>Sí</strong></p>
+                <p>Acceso a Pegatinas y Fondos: <strong>Premium</strong></p>
+                <p>Acceso a Eventos: <br><strong>Sí, Descuentos y Prioridad</strong></p>
+                <p>Sin anuncios: <strong>Sí</strong></p>
+                <p>Monedas PAWTS mensuales: <strong>Sí</strong></p>
+            </div>
+        </div>
+        <a href="#" class="adquierelos">¡ADQUIÉRELOS EN LA APP!</a>
+        <h1>Packs Adicionales de Perfiles de Mascotas</h1>
+        <div class="info-packs">
+            <h2>Flexibilidad para Cuidadores con Más Mascotas</h2>
+            <p>
+                En PAWTS entendemos que cada mascota es única y merece un espacio dedicado.
+                Si tu plan actual no cubre todas tus necesidades porque tienes más mascotas de las incluidas, te
+                ofrecemos una solución fácil y accesible: <span class="highlight">Packs Adicionales de Perfiles</span>. Estos packs te permiten
+                ampliar el número de perfiles disponibles para gestionar de manera eficiente la información y
+                cuidado de cada uno de tus compañeros animales.
+            </p>
+            <br>
+            <p>
+                <span class="highlight">
+                    Con esta opción, puedes personalizar tu experiencia según la cantidad de mascotas que tengas, sin
+                    necesidad de cambiar de membresía.
+                </span>
+                Tanto si eres un cuidador con varias mascotas en casa, como si 
+                diriges una granja, un centro ecuestre o incluso un zoológico, los packs adicionales son perfectos
+                para cubrir todas tus necesidades.                
+            </p>    
+        </div>
+        <h2>Cada perfil de mascota adicional te da acceso a todas las funcionalidades disponibles en tu plan actual, incluyendo:</h2>
+        <div class="incluyen-container">
+            <div class="incluyen">
+                <p>Registro detallado con nombre, foto, especie, raza, alergias y más.</p>
+            </div>
+            <div class="incluyen">
+                <p>Acceso al historial médico completo, con vacunas, tratamientos y revisiones.</p>
+            </div>
+            <div class="incluyen">
+                <p>Control económico personalizado para cada usuario.</p>
+            </div>
+            <div class="incluyen">
+                <p>Notas y recordatorios para citas y cuidados especiales</p>
+            </div>
+        </div>
+        <h3>
+            ¡No hay limitaciones!<br>
+            Los perfiles adicionales se integran perfectamente en tu suscripción, para que
+            cada mascota reciba el cuidado y la atención que merece.
+        </h3>
+        <p style="color: #33C096">
+        Importante saber, que en caso de no disponer de una membresía, estos packs no te dan los 
+        mismos beneficios ni derechos de un plan Premium o Pro.
+        </p>
+        <div class="container">
+            <div class="funciones-container">
+                <div class="funcion">
+                    <h4>Ampliación Automática</h4>
+                    <p>Una vez adquirido un pack, el número de perfiles disponibles en tu cuenta se actualiza automáticamente, sin interrupciones.</p>
+                    <br><br><br>
+                    </div>
+                <div class="funcion">
+                    <h4>Compra de Packs</h4>
+                    <p>Los packs están disponibles en la tienda de PAWTS y pueden adquirirse en modalidad mensual o anual.</p>
+                    <div class="extra-finfo">Los usuarios <span class="highlight">Premium</span> pueden añadir perfiles en paquetes pequeños para complementar sus 3 perfiles iniciales</div>
+                    <div class="extra-finfo">Los usuarios <span class="highlight">Pro</span> pueden adquirir packs más grandes para gestionar decenas de animales en granjas, hípicas o zoológicos</div>
+                </div>
+                <div class="funcion">
+                    <h4>Gestión Sencilla</h4>
+                    <p>Si decides no renovar un pack, podrás ajustar tu lista de mascotas al límite de perfiles permitido eliminando perfiles antiguos o no necesarios.</p>
+                    <p>En caso de no renovar, los perfiles adicionales se eliminarán automáticamente de tu cuenta.</p>
+                </div>
+            </div>
+            <h1>Packs Disponibles</h1>
+        </div>
+    <?php include '../resources/footer.php'; ?>
+</body>
+</html>
