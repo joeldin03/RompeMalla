3bb9a15d8f5cb2aaa17ccdac502473f0c6f832be
joeldin03 <jlozanobarbancho22@outlook.com>
2025-02-18 03:06:27 +0100

    falta arreglar la priemra parte

---
+++ pages/historia.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historia - PAWTS</title>
+    <link rel="stylesheet" href="/RompeMalla/css/historia.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/logo chiquito.png">
+</head>
+<body>
+    <?php include '../resources/header.php'; ?>
+
+    <div class="hero">
+        <h1>Nuestra Historia</h1>
+        <p><strong> PAWTS nació del <span class="highlight">amor</span> y la <span>pasión</span> que sentimos por nuestras mascotas.</strong></p>
+        <p>
+            Todo comenzó con la idea de facilitar la vida de los dueños de mascotas, ofreciéndoles una herramienta que les permitiera 
+            organizar y cuidar de sus amigos los animales de la mejor manera posible.
+        </p>
+        <p>
+            Nuestro equipo se unió con un objetivo en común:<br>
+            <strong>Crear una app que ayudara a mejorar la vida de las mascotas y sus dueños.</strong>
+        </p>
+        <p>Después de meses de trabajo, dedicación y muchas tazas de café, PAWTS se convirtió en una realidad</p>
+    </div>
+    <div class="mv-container">
+        <h1>Misión y Valores</h1>
+        <p>
+            Facilitar la vida de los dueños de mascotas mediante una plataforma completa que ofrezca organización, 
+            seguimiento y herramientas para el <strong>bienestar integral de los animales</strong>. 
+        </p>
+        <p>
+            Aspiramos a ser la plataforma de referencia mundial en el cuidado de mascotas, mejorando su calidad de vida y creando 
+            una comunidad global de <strong>dueños conscientes y conectados</strong>.
+        </p>
+        <div>
+            <img src="../img/FLORES 1.png" alt="flores izquierda">
+            <img src="../img/FLORES 2.png" alt="flores derecha">
+        </div>
+    </div>
+    <div class="unete-container">
+        <h2 style="color: #9CFFDB;">¿Te gustaría trabajar con nosotros?</h2>
+        <p style="color: #00291D;">
+            En PAWTS, siempre estamos buscando personas apasionadas por la tecnología y el bienestar animal. 
+            Si quieres formar parte de un equipo innovador y contribuir al cuidado de las mascotas, ¡nos encantaría conocerte!
+            Descubre nuestras oportunidades de empleo y cómo puedes unirte a nosotros.
+        </p>
+        <div class="unete">
+            <button>Únete al equipo PAWTS</button>
+            <img src="../img/ovillo.png" alt="ovillo rosa">
+        </div>
+    </div>
+    <?php include '../resources/footer.php'; ?>
+</body>
+</html>
