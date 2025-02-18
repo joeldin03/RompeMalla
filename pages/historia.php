5de7926a387015102605ed1950db6405baec0d93
Miguel Martin <miguelmartinand@gmail.com>
2025-02-18 20:43:00 +0100

    minor fixes on historia.php

--- pages/historia.php
+++ pages/historia.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,30 +9,34 @@
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/logo chiquito.png">
 </head>
+
 <body>
     <?php include '../resources/header.php'; ?>
 
     <div class="hero">
-        <h1>Nuestra Historia</h1>
-        <p><strong> PAWTS nació del <span class="highlight">amor</span> y la <span>pasión</span> que sentimos por nuestras mascotas.</strong></p>
-        <p>
-            Todo comenzó con la idea de facilitar la vida de los dueños de mascotas, ofreciéndoles una herramienta que les permitiera 
-            organizar y cuidar de sus amigos los animales de la mejor manera posible.
-        </p>
-        <p>
-            Nuestro equipo se unió con un objetivo en común:<br>
-            <strong>Crear una app que ayudara a mejorar la vida de las mascotas y sus dueños.</strong>
-        </p>
-        <p>Después de meses de trabajo, dedicación y muchas tazas de café, PAWTS se convirtió en una realidad</p>
+        <div class="hero-text">
+            <h1>Nuestra Historia</h1>
+            <img src="../img/huesito rosa.png" style="width: 55px;">
+            <p><strong> PAWTS nació del <span class="highlight">amor</span> y la <span>pasión</span> que sentimos por nuestras mascotas.</strong></p>
+            <p>
+                Todo comenzó con la idea de facilitar la vida de los dueños de mascotas, ofreciéndoles una herramienta que les permitiera
+                organizar y cuidar de sus amigos los animales de la mejor manera posible.
+            </p>
+            <p>
+                Nuestro equipo se unió con un objetivo en común:<br>
+                <strong>Crear una app que ayudara a mejorar la vida de las mascotas y sus dueños.</strong>
+            </p>
+            <p>Después de meses de trabajo, dedicación y muchas tazas de café, PAWTS se convirtió en una realidad</p>
+        </div>
     </div>
     <div class="mv-container">
         <h1>Misión y Valores</h1>
         <p>
-            Facilitar la vida de los dueños de mascotas mediante una plataforma completa que ofrezca organización, 
-            seguimiento y herramientas para el <strong>bienestar integral de los animales</strong>. 
+            Facilitar la vida de los dueños de mascotas mediante una plataforma completa que ofrezca organización,
+            seguimiento y herramientas para el <strong>bienestar integral de los animales</strong>.
         </p>
         <p>
-            Aspiramos a ser la plataforma de referencia mundial en el cuidado de mascotas, mejorando su calidad de vida y creando 
+            Aspiramos a ser la plataforma de referencia mundial en el cuidado de mascotas, mejorando su calidad de vida y creando
             una comunidad global de <strong>dueños conscientes y conectados</strong>.
         </p>
         <div>
@@ -42,15 +47,16 @@
     <div class="unete-container">
         <h2 style="color: #9CFFDB;">¿Te gustaría trabajar con nosotros?</h2>
         <p style="color: #00291D;">
-            En PAWTS, siempre estamos buscando personas apasionadas por la tecnología y el bienestar animal. 
+            En PAWTS, siempre estamos buscando personas apasionadas por la tecnología y el bienestar animal.
             Si quieres formar parte de un equipo innovador y contribuir al cuidado de las mascotas, ¡nos encantaría conocerte!
             Descubre nuestras oportunidades de empleo y cómo puedes unirte a nosotros.
         </p>
         <div class="unete">
-            <button>Únete al equipo PAWTS</button>
+            <a href="contacto.php">Únete al equipo PAWTS</a>
             <img src="../img/ovillo.png" alt="ovillo rosa">
         </div>
     </div>
     <?php include '../resources/footer.php'; ?>
 </body>
+
 </html>
--- resources/footer.php
+++ resources/footer.php
@@ -13,7 +13,7 @@
   
         <div class="links">
             <a href="/RompeMalla/pages/privacidad.php">Política de Privacidad</a>
-            <a href="/RompeMalla/pages/equipo.php">Sobre Nosotros</a>
+            <a href="/RompeMalla/pages/historia.php">Sobre Nosotros</a>
             <a href="/RompeMalla/pages/contacto.php">Contáctanos</a>
             <a href="/RompeMalla/pages/FAQ.php">Centro de Ayuda</a>
         </div>
@@ -24,7 +24,7 @@
             <a href="https://www.tiktok.com">
                 <img src="/RompeMalla/img/Social Icons tiktok.png" alt="TikTok">
             </a>
-            <a href="https://www.instagram.com/">
+            <a href="https://www.instagram.com/pawtsmascotas/">
                 <img src="/RompeMalla/img/Social Icons instagram.png" alt="Instagram">
             </a>
         </div>
